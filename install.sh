b30b7dd1d49d5c960b28b74ef61d5135dd403f92
root <root@DESKTOP-TKJ38QL.localdomain>
2021-03-29 14:59:27 +0900

    :+1: Update : 機能の修正 権限の修正

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd /opt
 sudo git clone https://github.com/yn4k4nishi/git-prefix-tool.git
 
-sudo chmod 700 git-prefix-tool/git-prefix-tool.sh
-sudo chmod 700 git-prefix-tool/python/*.py
+sudo chmod 777 git-prefix-tool/git-prefix-tool.sh
+sudo chmod 777 git-prefix-tool/python/*.py
 
 sudo ln git-prefix-tool/git-prefix-tool.sh /usr/local/bin/git-prefix-tool
 
