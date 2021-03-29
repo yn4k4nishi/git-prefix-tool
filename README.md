2ecdc69fabdd39bf6e90a09d5952f686c2183697
Yusuke Nakanishi <yn4k4nishi.kit@gmail.com>
2021-03-29 13:35:36 +0900

    :sparkles: Feature : 新機能の実装 インストールスクリプトの追加

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # git-prefix-tool
+
+## install
+```bash
+wget -qO - https://raw.githubusercontent.com/yn4k4nishi/git-prefix-tool/dev/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -3,9 +3,16 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo $SCRIPT_DIR
 
-# sudo apt intstall python3
+sudo apt intstall python3
 
-# cd /opt
-# sudo git clone https://github.com/yn4k4nishi/git-prefix-tool.git
+cd /opt
+sudo git clone https://github.com/yn4k4nishi/git-prefix-tool.git
 
-# cd $SCRIPT_DIR
+sudo chmod 700 git-prefix-tool/git-prefix-tool.sh
+
+ln -s git-prefix-tool/git-prefix-tool.sh /usr/local/bin/git-prefix-tool
+
+cd
+source .bashrc
+
+cd $SCRIPT_DIR
