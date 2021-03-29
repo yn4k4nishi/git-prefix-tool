a5588a256ec1f5156e888a59e704efd6e22d7349
Yusuke Nakanishi <yn4k4nishi.kit@gmail.com>
2021-03-29 13:06:00 +0900

    :sparkles: Feature : 新機能の実装 インストールスクリプトの追加

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+echo $SCRIPT_DIR
+
+# sudo apt intstall python3
+
+# cd /opt
+# sudo git clone https://github.com/yn4k4nishi/git-prefix-tool.git
+
+# cd $SCRIPT_DIR
