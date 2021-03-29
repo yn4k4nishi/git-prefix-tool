774383fcc92f616d0e760001fba9bb41c0358d5c
Yusuke Nakanishi <yn4k4nishi.kit@gmail.com>
2021-03-29 12:40:37 +0900

    :sparkles: Feature : 新機能の実装 pushの追加

--- python/main.py
+++ python/main.py
@@ -3,10 +3,11 @@
 import curses
 from add import add
 from commit import commit
+from push import push
 
 def main(stdscr):
-    add(stdscr)
+    add(stdscr) 
     commit(stdscr)
+    push(stdscr)
 
 curses.wrapper(main)
-
---
+++ python/push.py
@@ -0,0 +1,33 @@
+import curses
+import os 
+
+def push(stdscr):
+    stdscr.clear()
+    stdscr.keypad(True)
+    curses.use_default_colors()
+    curses.init_pair(2, curses.COLOR_YELLOW, -1)
+
+    input = ''
+    while True:
+        stdscr.addstr(0,0,"Do you push it ? (Y/n) : " + input, curses.color_pair(2))
+
+        stdscr.refresh()
+        key = stdscr.getkey()
+
+        if len(key) == 1 and ord(key) == 27: # ESC
+            break
+        elif key == '\n':
+            if(input == 'y' or input == 'Y' or input == ''):
+                os.system('git push')
+                break
+            elif (input == 'n' or input == 'N'):
+                break
+        elif key == 'y' or key == 'Y':
+            input = key
+        elif key == 'n' or key == 'N':
+            input = key
+        elif key == 'KEY_BACKSPACE':
+            input = ''
+
+if __name__ == "__main__":
+    curses.wrapper(push)
