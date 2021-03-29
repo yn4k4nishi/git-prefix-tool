b73fd1df4ebbcd343951f4fed998b67f71ff4b58
Yusuke Nakanishi <yn4k4nishi.kit@gmail.com>
2021-03-29 14:33:07 +0900

    fix

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ echo $SCRIPT_DIR
 sudo apt intstall python3
 
 cd /opt
-sudo git clone https://github.com/yn4k4nishi/git-prefix-tool.git
+sudo git clone -b dev https://github.com/yn4k4nishi/git-prefix-tool.git
 
+sudo chmod 777 git-prefix-tool/git-prefix-tool.sh
 sudo chmod 777 git-prefix-tool/git-prefix-tool.sh
 
 sudo ln git-prefix-tool/git-prefix-tool.sh /usr/local/bin/git-prefix-tool
